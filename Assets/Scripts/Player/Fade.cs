2e5ce0db1b8b563d69a1a8b69f7b2af2208bcffb
Kamil Butryn <kaybenot@gmail.com>
2024-10-26 03:38:01 +0200

    Fade

--- Assets/Scripts/Player/Fade.cs
+++ Assets/Scripts/Player/Fade.cs
@@ -8,7 +8,7 @@ public class Fade : MonoBehaviour
     public Action OnFadedIn { get; set; }
 
     public static Fade Instance;
-
+    
     public void Out(float time)
     {
         StartCoroutine(IOut(time));
@@ -19,13 +19,46 @@ public class Fade : MonoBehaviour
         StartCoroutine(IIn(time));
     }
 
+    [ContextMenu("Fade Out")]
+    private void OutEditor()
+    {
+        Out(2f);
+    }
+
+    [ContextMenu("Fade In")]
+    private void InEditor()
+    {
+        In(2f);
+    }
+
     IEnumerator IOut(float time)
     {
+        RenderSettings.fog = true;
         
+        var timer = 0f;
+        while (timer < time)
+        {
+            yield return new WaitForEndOfFrame();
+            timer += Time.deltaTime;
+            RenderSettings.fogDensity = timer / time;
+        }
+        RenderSettings.fogDensity = 10f;
+        
+        OnFadedOut?.Invoke();
     }
     
     IEnumerator IIn(float time)
     {
+        var timer = 0f;
+        while (timer < time)
+        {
+            yield return new WaitForEndOfFrame();
+            timer += Time.deltaTime;
+            RenderSettings.fogDensity = 1 - (timer / time);
+        }
         
+        RenderSettings.fog = false;
+        
+        OnFadedIn?.Invoke();
     }
 }
